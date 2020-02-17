7119ebd7f548a69b7cf4432f24adbe8a8c0b6ba4
Samia Islam Oyse <60336691+samiaislam16@users.noreply.github.com>
2020-02-17 13:54:37 +0600

    Add files via upload

---
+++ startpage.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+	<form  method="POST">
+
+		Name:
+		<input type="text" name="name">
+		ID:<input type="text" name="id">
+
+		<input type="submit" name="submit">
+
+
+
+	</form>
+
+	<a href="next.php"> next</a>
+
+
+
+</body>
+</html>
